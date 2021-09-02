451fdb27d1009dda52eb2842b4bc059979ef242d
Szymon Balawajder <63589837+szymonbcoding@users.noreply.github.com>
2021-09-02 11:39:15 +0200

    Init commit

--- szum_bw.py
+++ szum_bw.py
@@ -2,103 +2,82 @@ from PIL import Image
 import PIL
 import math
 
-#DO ZMIANY, JEŚLI ARKUSZ SIĘ ZMIENI
-x_mm = 118
-y_mm = 58
-
-square_side_mm = 20
-
-def find_odd_pixel(v: int, avr1: float) -> bool:
+def calc_avr(photo: PIL.Image.Image) -> list:
     
-    if(abs(v - avr1) > 50):
-        return True
-    else:
-        return False
-
-def calc_avr(photo: PIL.Image.Image) -> float:
-    
+    avr_list = []
     w, h = photo.size
     n = w*h
 
-    s = 0
+    for i in range(3):
+        s = 0
+        for j in range(w):
+            for k in range(h):
+                s += photo.getpixel((j, k))[i]
+        avr_list.append(s/n) 
 
-    for j in range(w):
-        for k in range(h):
-            s += photo.getpixel((j, k))
+    return avr_list 
 
-    return (s/n)
+def calc_deviation(photo: PIL.Image.Image, avr_list: list) -> list:
+    
+    dev_list = []
+
+    w, h = photo.size
+    n = w*h
+
+    for i in range(3):
+        s = 0
+        for j in range(w):
+            for k in range(h):
+                s += (photo.getpixel((j, k))[i] - avr_list[i]) ** 2
+        dev_list.append(math.sqrt(s/n)) 
+    
+    return dev_list
+
+def calc_percent_deviation(dev_list: list, avr_list: list) -> list:
+
+    perc_dev_list = []
+
+    for i in range(3):
+        perc_dev_list.append(dev_list[i]/avr_list[i] * 100)
+
+    return perc_dev_list
 
 ############################################
 #def main(photo: PIL.Image.Image, mode: int) -> list:
 def main():
 
     #wstaw ścieżkę do zdjęcia
-    im = Image.open("cropped_images/CD2.JPG")
+    im = Image.open("cropped_images/SD.JPG")
     x,y = im.size
 
-    square_side_px = math.floor(x * square_side_mm / x_mm)
+    px = im.convert("RGB")
 
-    px = im.convert("L")
+    results_list = []
 
-    v_quarter = math.floor(y/4)
+    supp_list = []
 
-    #obliczanie średniej wartości pikseli pierwszego wiersza
-    sum1 = 0
-    for i in range(x):
-            sum1 += px.getpixel((i, 0))
+    avr_supp = calc_avr(px)
+    dev_supp = calc_deviation(px, avr_supp)
 
-    avr1 = sum1/x
+    supp_list.append(avr_supp)
+    supp_list.append(dev_supp)
+    supp_list.append(calc_percent_deviation(dev_supp, avr_supp))
 
-    h_pointer = -1
+    results_list.append(supp_list)
 
-    for i in range(x):
-
-        j = px.getpixel((i, v_quarter))
-
-        if(find_odd_pixel(j, avr1)):
-            h_pointer = i + 0.25 * square_side_px
-            break
-    
-    crop_coords = []
-
-    for i in range(10):
-
-        supp_list = []
-
-        if(i < 5):
-            q = 1
-        elif(i >= 5):
-            q = 3
-
-        #left, top, right, down
-        supp_list.append(h_pointer + (i % 5) * square_side_px)
-        supp_list.append(q * v_quarter - 0.2 * square_side_px)
-        supp_list.append(h_pointer + ((i % 5) + 0.5) * square_side_px)
-        supp_list.append(q * v_quarter + 0.2 * square_side_px)
-
-        crop_coords.append(supp_list)
-    
-
-    cropped_images = []
-
-    for i in range(10):
-        cropped_images.append(px.crop((crop_coords[i][0], crop_coords[i][1], crop_coords[i][2], crop_coords[i][3])))
-        #cropped_images[i].save('bw/' + str(i) + '.JPG')
-
-    labels = ["black", "white"]
-
-    for i in range(10):
-
-        if(i < 5):
-            print("Black" + str(i))
-        elif(i >= 5):
-            print("White" + str(i - 5))
-
-        a = calc_avr(cropped_images[i])
-
-        print("Average:", a)
-        print("")
+    print(results_list)
+    #wyświetlenie i interpretacja wyników
 
 if __name__ == "__main__":
     #main(photo, mode)
     main()
+
+    
+
+        
+
+
+
+
+
+
