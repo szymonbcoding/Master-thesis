03fef820d4fe863c571f6abf7566e14307b3d514
Szymon Balawajder <sbalawajder@man.poznan.pl>
2021-09-03 14:49:57 +0200

    Update polish messages

--- program_wstepny.py
+++ program_wstepny.py
@@ -28,13 +28,11 @@ elif(0.705 <= y/x < 0.79):
     mode = 34
     w = w34
 else:
-    print("Wrong image resolution.")
-
-#mode = 23
+    print("Nieobslugiwana proporcja obrazu.")
 
 wb = load_workbook(filename = 'coordinates_v2.xlsx')
 
-#resolution tests
+#otworz odpowiedni arkusz
 if(mode == 23):
     sheet = wb['23']
 elif(mode == 34):
@@ -51,19 +49,10 @@ for i in range(2,19):
             if(k):
                 crop_supp.append(k)
             if(j==5):
-                #print(crop_supp)
                 crop_list.append(crop_supp)
-
-"""
-print("x:", x)
-print("y:", y)  
-print(im.getpixel((0,0)))
-print(im.getpixel((x,y)))   
-"""
 
 
 for m in range(0, len(crop_list)):
-    #save_list.append(im.crop((convertMmToPx(crop_list[x][1],w,x), convertMmToPx(crop_list[x][2],h,y), convertMmToPx(crop_list[x][3],w,x), convertMmToPx(crop_list[x][4],h,y))))
     save_list.append(im.crop((math.floor(crop_list[m][0]*x/w), math.floor(crop_list[m][1]*y/h), math.floor(crop_list[m][2]*x/w), math.floor(crop_list[m][3]*y/h))))
     save_list[m].save('cropped_images/' + labels[m] + '.JPG')
 
