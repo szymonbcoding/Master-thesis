e2a1dd202b28dc9c9990caf5c7f4e63326fb89b8
Szymon Balawajder <63589837+szymonbcoding@users.noreply.github.com>
2021-12-03 16:53:57 +0100

    Deleted cleaning unused sheet

--- clean_sh.py
+++ clean_sh.py
@@ -1,24 +1,12 @@
 from openpyxl import load_workbook
 
 def main():
-
-    wb = load_workbook("dane_szczegolowe.xlsx")
-
-
-    for sheet in wb.worksheets:
-        for i in range(4, 100):
-            for j in range(1, 90):
-                
-                sheet.cell(row = i, column = j).value = None
-                
-    wb.save("dane_szczegolowe.xlsx")
-    wb.close()
     
     wb2 = load_workbook("komunikat.xlsx")   
     
     sheet2 = wb2['Arkusz1']   
     
-    for i in range(1, 40):
+    for i in range(1, 300):
         for j in range(2, 100):
                 
             sheet2.cell(row = i, column = j).value = None
